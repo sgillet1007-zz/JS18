bf8c74840336d784af4be395cdb0d2b2aa4b3492
sgillet1007 <samuelgillet@hotmail.com>
2015-07-10 11:44:25 -0600

    bootstrap frontend

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 $(document).on('ready', function() {
  
 
+var Quote =(function(){
+	var quoteArray = [];
+	var quoteCounter = 1000;
 
+	var currentQuote = function(quote, author){
+		this.quote = quote;
+		this.author = author;
+		quoteArray.push(this);
+	}
+	return currentQuote;
+});
 
+var quoteRef = 'quote' + toString(Quote.quoteCounter) //how do I access static quote counter from outside Quote
+console.log(quoteRef);
 
-
-
-
-
-
-
-
-
+$('.submit').on('click', function(){
+	// on click create a new instance of Quote pass .val() from both inputs as parameters to constructor.
+	 var quoteRef = quote + Quote.quoteCounter
+	 quoteRef = new Quote($('.quoteInput').val(),$('.authorInput').val());
+	 quoteRef++
+	 console.log(quoteRef);
+	});
 
 });
